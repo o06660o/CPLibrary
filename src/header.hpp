679fecf35e6f53638eff9f22ecd54e5d126ac014
o06660o <2957531723@qq.com>
2025-09-28 09:12:50 +0800

    first commit

---
+++ src/header.hpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+
+#define ALL(a) (a).begin(), (a).end()
+#define FOR(i, a, b) for (int i = (a); i < (b); i++)
+#define PUSHB push_back
+using namespace std;
+using ll = long long;
+using ull = unsigned long long;
+using i128 = __int128_t;
+using u128 = __uint128_t;
+using pii = pair<int, int>;
+using pll = pair<ll, ll>;
+using pli = pair<ll, int>;
+using pil = pair<int, ll>;
---
+++ test/oj/dsu.test.cpp
@@ -0,0 +1,22 @@
+#define PROBLEM "https://judge.yosupo.jp/problem/unionfind"
+#include <bits/stdc++.h>
+using namespace std;
+
+#include "../../src/ds/dsu.inc"
+
+int main() {
+  cin.tie(nullptr)->sync_with_stdio(false);
+  int n, q;
+  cin >> n >> q;
+  DSU dsu(n);
+  while (q--) {
+    int op, u, v;
+    cin >> op >> u >> v;
+    if (op == 0) {
+      dsu.merge(u, v);
+    } else if (op == 1) {
+      cout << dsu.same(u, v) << "\n";
+    }
+  }
+  return 0;
+}
